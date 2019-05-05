2a7b016c3821521d1448df3f67c781fcdbf24973
William Calderipe <wcalderipe@gmail.com>
2019-05-05 14:12:32 +1000

    Initiliaze project with Clojure 1.10.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# Snoopy Bot
+
+Snoopy is Charlie's friend and also a Slack bot built with Clojure.
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject snoopy "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :main ^:skip-aot snoopy.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
