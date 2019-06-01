38a5f4686a147c71e8b3a38cd63bd576ee406421
William Calderipe <wcalderipe@atlassian.com>
2019-06-01 17:27:43 +1000

    Add a base concept of commands in the core namespace

--- src/snoopy/core.clj
+++ src/snoopy/core.clj
@@ -1,7 +1,40 @@
 (ns snoopy.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;; Idea
+;;
+;; Command:
+;; They represent a single user action identified by a prefix regex
+;; (i.e. ping).  The commands will be stored into an atom map where
+;; the key is the lockdown prefix and the value is the handler.
+;;
+;; Before store the command, we should lockdown the regex to prevent
+;; collisions. The lockdown mechanism will prefix and suffix the given
+;; regex with `^` and `$`. For instance, the prefix regex `#"ping"` is
+;; stored as `#"^ping$"`.
+;;
+;; Inside a chat, like Slack, commands are identified by the prefix
+;; `!`. The ping example will only be triggered when a user say
+;; `!ping`.
+;;
+;; Commands are ment to be used exclusive by Slack RTM API which means
+;; we can only send strings. That said, the expected return for any
+;; command should be a string that will be send to the chat
+;; application. If enconding is required, it should be done in above
+;; layers.
+;;
+;; TODO: command arguments (use argv parse as base)
+
+(def commands (atom {}))
+
+(defn lockdown-str-prefix
+  "Lockdown a str-prefix envolving it with `^` and `$` to generate a
+  strict regex to prevent collisions."
+  [str-prefix]
+  (let [re-str (str str-prefix)]
+    (str "^" re-str "$")))
+
+(defn add-cmd!
+  "Store a new command str-prefix and handler."
+  [str-prefix handler]
+  (swap! commands conj {(lockdown-str-prefix str-prefix) handler}))
--- test/snoopy/core_test.clj
+++ test/snoopy/core_test.clj
@@ -1,7 +1,15 @@
 (ns snoopy.core-test
   (:require [clojure.test :refer :all]
-            [snoopy.core :refer :all]))
+            [snoopy.core :refer [add-cmd! commands]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn foo-cmd [_]
+  "I am foo!")
+
+(defn reset []
+  (reset! commands {}))
+
+(deftest add-cmd!-test
+  (testing "lockdown string prefix"
+    (reset)
+    (add-cmd! "foo" foo-cmd)
+    (is (not (nil? (get @commands "^foo$"))))))
