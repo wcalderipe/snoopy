1381b1e5ac7914432d268978a1aa650df7826ceb
William Calderipe <wcalderipe@atlassian.com>
2019-06-02 11:49:59 +1000

    Add Slack processor component
    This commit add a processor component, purpose is establish a
    connection with Slack and process all incoming messages. It will be
    guided by the following rules:
    - If the incoming message is a command (starts with a bang and is
      valid command)
    - Parse the message
    - Evaluate and get the result of the command
    - Send the result to the same channel
    
    Also introduce the outcoming messages channel in the Slack namespace.
    
    The next steps are, possibly, improve the testability of
    slack-processor and slack namespaces.

--- src/snoopy/slack.clj
+++ src/snoopy/slack.clj
@@ -1,7 +1,8 @@
 (ns snoopy.slack
-  (:require [clojure.core.async :as async :refer [>! <! close! chan pub sub go go-loop]]
+  (:require [clojure.core.async :as async :refer [>! <! close! chan pub sub go go-loop unsub-all]]
             [clj-http.client :as client]
-            [gniazdo.core :as ws]))
+            [gniazdo.core :as ws]
+            [cheshire.core :as json]))
 
 ;; Stablish a connectin with Slack RTM via WebSocket, messages will have
 ;; two directions incoming and outcoming which will be handled internally
@@ -25,45 +26,71 @@
   [incoming-ch]
   (fn [raw-message]
     (let [message (json/parse-string raw-message true)]
+      (println "incoming event:" message)
       (when-not (contains? excluded-event-types (:type message))
         (go (>! incoming-ch message))))))
 
+(defn- build-outcoming-channel
+  [socket]
+  (let [ch (chan)]
+    (go-loop []
+      (when-let [event (<! ch)]
+        (println "outcoming event:" event)
+        (ws/send-msg socket (json/generate-string event))
+        (recur)))
+    ch))
+
 (defn- ws-connect
   [url]
-  (let [incoming-ch (chan)
-        socket      (ws/connect url :on-receive (receive-handler incoming-ch))
-        publication (pub incoming-ch #(:type %))]
+  (let [incoming-ch  (chan)
+        socket       (ws/connect url :on-receive (receive-handler incoming-ch))
+        publication  (pub incoming-ch #(:type %))
+        outcoming-ch (build-outcoming-channel socket)]
     {:incoming-ch incoming-ch
+     :outcoming-ch outcoming-ch
      :socket socket
      :publication publication}))
+
+(defn start!
+  [token]
+  (let [ws-url (:url (slack-connect token))
+        conn   (ws-connect ws-url)]
+    conn))
 
 (defn sub-to-event
   [publication type ch]
   (sub publication type ch))
 
-(defn start!
-  token
-  (let [ws-url (:url (slack-connect token))
-        conn   (ws-connect ws-url)]
-    conn))
+(defn pub-event
+  [outcoming-ch event]
+  (go (>! outcoming-ch event)))
 
 (defn close-connection
   [conn]
   (close! (:incoming-ch conn))
+  (close! (:outcoming-ch conn))
+  (unsub-all (:publication conn))
   (ws/close (:socket conn)))
 
 (comment
   (def token "xoxb-foo-bar-baz")
 
-  (defn build-message-channel
-    []
+  (defn build-print-channel
+    [prefix]
     (let [ch (chan)]
       (go-loop []
         (when-let [message (<! ch)]
-          (println "message" message)
+          (println (str prefix ":") message)
           (recur)))
       ch))
 
   (def conn (start! token))
   (close-connection conn)
-  (sub-to-event (:publication conn) "message" (build-message-channel)))
+
+  (sub-to-event (:publication conn) "message" (build-print-channel "message"))
+  (sub-to-event (:publication conn) "pong" (build-print-channel "ping-pong"))
+
+  (pub-event (:outcoming-ch conn) {:type :ping})
+  (pub-event (:outcoming-ch conn) {:type :message
+                                   :channel "CHANNEL_ID"
+                                   :text "lorem ipsum"}))
---
+++ src/snoopy/slack_processor.clj
@@ -0,0 +1,42 @@
+(ns snoopy.slack-processor
+  (:require [clojure.core.async :refer [>! <! chan go go-loop]]
+            [snoopy.core :as bot]
+            [snoopy.slack :as slack]))
+
+;; Slack Processor Component
+;;
+;; This component purpose is establish a connection with Slack RTM API
+;; and process all incoming messages. It will be guided by the
+;; following rules:
+;;
+;; - If the incoming message is a command (starts with a bang and is
+;;   valid command)
+;; - Parse the message
+;; - Evaluate and get the result of the command
+;; - Send the result to the same channel
+
+(defn build-incoming-message-channel []
+  (let [ch (chan)]
+    (go-loop []
+      (when-let [message (<! ch)]
+        (when-let [parsed-cmd (bot/cmd-parse (:text message))]
+          (println "***" (bot/cmd-resolve parsed-cmd))
+          (slack/pub-event (:outcoming-ch @conn) {:type :message
+                                                  :channel (:channel message)
+                                                  :text (bot/cmd-resolve parsed-cmd)}))
+        (recur)))
+    ch))
+
+(def conn (atom nil))
+
+(defn start [token]
+  (bot/add-cmd! "ping" (fn [] "pong!"))
+
+  (let [c           (slack/start! token)
+        publication (:publication c)]
+    (reset! conn c)
+    (slack/sub-to-event publication "message" (build-incoming-message-channel))))
+
+(defn stop []
+  (slack/close-connection @conn)
+  (reset! conn nil))
