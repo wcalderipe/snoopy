d60c487a031d037dc2987eca583cf6f48b12ffde
William Calderipe <wcalderipe@atlassian.com>
2019-06-02 10:26:11 +1000

    Add function to resolve a parsed command

--- test/snoopy/core_test.clj
+++ test/snoopy/core_test.clj
@@ -1,8 +1,12 @@
 (ns snoopy.core-test
   (:require [clojure.test :refer :all]
-            [snoopy.core :refer [add-cmd! commands cmd-parse]]))
+            [snoopy.core :refer [->ParsedCommand
+                                 add-cmd!
+                                 commands
+                                 cmd-parse
+                                 cmd-resolve]]))
 
-(defn foo-cmd [_]
+(defn foo-cmd []
   "I am foo!")
 
 (defn reset []
@@ -22,4 +26,10 @@
     (is (nil? (cmd-parse "I am not a command!"))))
 
   (testing "returns nil if command is not found"
-    (is (nil? (cmd-parse "!not-me")))))
+    (is (nil? (cmd-parse "!not-me"))))
+
+  (testing "returns a parsed command"
+    (is (instance? snoopy.core.ParsedCommand (cmd-parse "!foo")))))
+
+(deftest cmd-resolve-test
+  (is (= "I am foo!" (cmd-resolve (->ParsedCommand "!foo" foo-cmd)))))
